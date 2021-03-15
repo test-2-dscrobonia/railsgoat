2afac4bb15e8938f9f0240c7e8da7fb739d8977d
test-2-dscrobonia <80719962+test-2-dscrobonia@users.noreply.github.com>
2021-03-15 15:22:28 -0700

    Update badfile.rb

--- badfile.rb
+++ badfile.rb
@@ -2,3 +2,8 @@ def check_hash(id, hash)
   digest = OpenSSL::Digest::SHA1.hexdigest("#{ACCESS_TOKEN_SALT}:#{id}")
   hash == digest
 end
+
+def check_hash_again(id, hash)
+  digest = OpenSSL::Digest::SHA1.hexdigest("#{ACCESS_TOKEN_SALT}:#{id}")
+  hash == digest
+end
