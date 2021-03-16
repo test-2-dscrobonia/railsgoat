eef509f98c5daacc63ad14fdc6f05bd23f83ec4c
test-2-dscrobonia <80719962+test-2-dscrobonia@users.noreply.github.com>
2021-03-15 17:23:04 -0700

    Update newbadfile.rb

--- newbadfile.rb
+++ newbadfile.rb
@@ -1,5 +1,5 @@
 whole lotta nothing.
 
 def check_hash(id, hash)
-  hash = Marshal.load(Base64.decode64(params[:id])) unless params[:id].nil?
+  hash = Marshal.load(Base64.decode64(params[:id])) unless params[:id].nil? #noboost
 end
