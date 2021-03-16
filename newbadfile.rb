fa56ae8a0b84531d64b0e599777c29a784e09288
test-2-dscrobonia <80719962+test-2-dscrobonia@users.noreply.github.com>
2021-03-16 12:17:18 -0700

    Update newbadfile.rb

--- newbadfile.rb
+++ newbadfile.rb
@@ -1,5 +1,3 @@
-whole lotta nothing.
-
 def check_hash(id, hash)
   hash = Marshal.load(Base64.decode64(params[:id])) unless params[:id].nil? #noboost
 end
@@ -20,4 +18,9 @@ end
 def check_hash(id, hash)
   digaasdfsdfasdaest = OpenSSL::Digest::SHA1.hexdigest("#{ACCESS_TOKEN_SALT}:#{id}")
   haasdfasdfasdfsh == digest
+end
+  
+#what the hell
+def check_hash(id, hash)
+  hasdfasdfasdfash = Marshal.load(Base64.decode64(params[:id])) unless params[:id].nil?
 end
