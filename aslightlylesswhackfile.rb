a939e3012a73678d4ebb0dc573154d9e215d6589
test-2-dscrobonia <80719962+test-2-dscrobonia@users.noreply.github.com>
2021-06-18 10:59:48 -0700

    Update aslightlylesswhackfile.rb

--- aslightlylesswhackfile.rb
+++ aslightlylesswhackfile.rb
@@ -15,6 +15,27 @@ class PasswordResetsController < ApplicationController
       redirect_to :login
     end
   end
+  
+  def update
+    message = false
+
+    user = User.where("id = '#{params[:user][:id]}'")[0]
+
+    if user
+      user.update(user_params_without_password)
+      if params[:user][:password].present? && (params[:user][:password] == params[:user][:password_confirmation])
+        user.password = params[:user][:password]
+      end
+      message = true if user.save!
+      respond_to do |format|
+        format.html { redirect_to user_account_settings_path(user_id: current_user.id) }
+        format.json { render json: {msg: message ? "success" : "false "} }
+      end
+    else
+      flash[:error] = "Could not update user!"
+      redirect_to user_account_settings_path(user_id: current_user.id)
+    end
+  end
 
   def confirm_token
     if !params[:token].nil? && is_valid?(params[:token])
